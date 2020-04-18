68bf1dbc97666d7b21f6fd3e26547dd778be9467
Nicolai H <60145008+nicolai-h@users.noreply.github.com>
2020-04-18 20:48:38 +0200

    Update Server.java

--- radamsa/assets/Server.java
+++ radamsa/assets/Server.java
@@ -23,7 +23,7 @@ public class Server {
             System.err.println("failed to listen to port: " + this.port);
             System.exit(1);
         }
-        System.out.println("succeded to listen to port: " + this.port);
+        System.out.println("succeeded to listen to port: " + this.port);
 
         while (true) {
             try {
@@ -62,7 +62,7 @@ public class Server {
                 // if returning user
                 else if (("GET / HTTP/1.1".equals(get)) && (has_read_id != -1)){
                     out.println(default_http + "\n\n");
-                    out.println("hello, old user!");
+                    out.println("hello, old user");
                 }
               
             }catch (IOException e){
